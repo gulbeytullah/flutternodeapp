5435f5613a7a41869c170d0dfd93882004740ccb
Beytullah GÜL <156790248+gulbeytullah@users.noreply.github.com>
2024-06-30 10:23:47 +0300

    making a dashbord

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutternodeapp/screens/dashboard.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() async{
-
   WidgetsFlutterBinding.ensureInitialized();
-
-
+  final dir = await getApplicationDocumentsDirectory();
+  await Hive.initFlutter(dir.path);
   runApp(const MyApp());
 }
 
--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+void main() async {
+  await Hive.initFlutter();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Dashboard(),
+    );
+  }
+}
 
 class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
@@ -9,88 +25,146 @@ class Dashboard extends StatefulWidget {
 }
 
 class DashboardState extends State<Dashboard> {
+  Box? noteBox;
+
+  @override
+  void initState() {
+    super.initState();
+    boxOpen();
+  }
+
+  void boxOpen() async {
+    noteBox = await Hive.openBox("notes");
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 53, 52, 121),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 38,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Image.asset(
-                'assets/icons/note.png',
-                width: 50,
-              ),
-              const Column(
-                children: [
-                  Text(
-                    'Not Defteri',
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      color: Colors.white,
+      body: noteBox != null
+          ? ValueListenableBuilder(
+              valueListenable: noteBox!.listenable(),
+              builder: (context, Box box, _) {
+                if (box.isEmpty) {
+                  return Center(
+                    child: Text(
+                      "No notes available.",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Poppins',
+                      ),
                     ),
+                  );
+                }
+
+                final values = box.values.toList();
+                values.sort((b, a) => a["date"].compareTo(b["date"]));
+
+                return SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 38,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Image.asset(
+                            'assets/icons/note.png',
+                            width: 50,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "Not Defteri",
+                                style: TextStyle(
+                                  fontFamily: "Poppins",
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                "${values.length} Adet Not",
+                                style: TextStyle(
+                                  fontFamily: 'Intel',
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.settings,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                        child: GridView.custom(
+                          physics: NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          gridDelegate: SliverQuiltedGridDelegate(
+                            crossAxisCount: 3,
+                            mainAxisSpacing: 4,
+                            crossAxisSpacing: 4,
+                            repeatPattern: QuiltedGridRepeatPattern.inverted,
+                            pattern: [
+                              QuiltedGridTile(2, 2),
+                              QuiltedGridTile(1, 1),
+                              QuiltedGridTile(1, 1),
+                            ],
+                          ),
+                          childrenDelegate: SliverChildBuilderDelegate(
+                            childCount: values.length,
+                            (context, index) => Container(
+                              decoration: BoxDecoration(
+                                color: Color.fromARGB(255, 53, 35, 94),
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              child: Stack(
+                                children: [
+                                  Text(
+                                    values[index]["title"],
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: "Poppins",
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                      values[index]["note"],
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontFamily: "Intel",
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  Text(
-                    '38 Adet Not',
-                    style: TextStyle(
-                      fontFamily: 'Intel',
-                      color: Colors.grey,
-                    ),
-                  ),
-                ],
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.settings,
-                  color: Colors.white,
-                ),
-              ),
-            ],
-          ),
-           SizedBox(
-            height: 10,
-          ),
-        GridView.custom(            
-              physics: NeverScrollableScrollPhysics(),   
-              shrinkWrap: true,          
-              gridDelegate: SliverQuiltedGridDelegate(
-                crossAxisCount: 3,
-                mainAxisSpacing: 4,
-                crossAxisSpacing: 4,
-                repeatPattern: QuiltedGridRepeatPattern.inverted,
-                pattern: [
-                  QuiltedGridTile(2, 2),
-                  QuiltedGridTile(1, 1),
-                  QuiltedGridTile(1, 1),
-                ],
-              ),
-              childrenDelegate: SliverChildBuilderDelegate(
-                (context, index) => Container(
-                  color: Color.fromARGB(255, 53, 35, 94),
-               ),
-             ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class Tile extends StatelessWidget {
-  final int index;
-
-  const Tile({super.key, required this.index});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Center(
-        child: Text('Tile $index'),
+                );
+              },
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color.fromARGB(255, 53, 52, 121),
+        onPressed: () {},
+        child: Icon(Icons.add),
       ),
     );
   }
